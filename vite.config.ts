6cacd583e7bd8da56932447e0e0f28f85366cf1b
Tollie Bonker <timosteen22@gmail.com>
2025-06-01 14:35:50 +0200

    Changed vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // Voeg hier de 'base' optie toe
+  base: "/mvdaa-stucwerk-scrolls/", // <<<<< DEZE REGEL IS BELANGRIJK!
   server: {
     host: "::",
     port: 8080,
